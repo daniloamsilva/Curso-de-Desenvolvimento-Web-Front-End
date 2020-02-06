aeb0d35d4685a80f68cde441c44b61c6b71c9bc3
daniloamsilva <danilo.augmonteiro@gmail.com>
2020-02-06 18:52:37 -0300

    Aulas JavaScript

--- Aulas JavaScript/App Game Mata Mosquito/jogo.js
+++ Aulas JavaScript/App Game Mata Mosquito/jogo.js
@@ -18,9 +18,9 @@ function posicaoRandomica(){
         document.getElementById('mosquito').remove();
 
         if(vidas > 3){
-            alert('Interromper o jogo (game over)');
+            window.location.href = 'fim_de_jogo.html';
         } else{
-            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png";
+            document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png';
             vidas++;
         }
     }
