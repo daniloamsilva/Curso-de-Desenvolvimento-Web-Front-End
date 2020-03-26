6d051143d183cbc2116902e57450dcbc37f31b52
daniloamsilva <danilo.augmonteiro@gmail.com>
2020-03-25 23:47:22 -0300

    Aula JavaScript

--- Aulas JavaScript Moderno(ES6) e OO/App Orçamento Pessoal/app.js
+++ Aulas JavaScript Moderno(ES6) e OO/App Orçamento Pessoal/app.js
@@ -55,6 +55,10 @@ class Bd {
 
         return despesas;
     }
+
+    pesquisar(despesa){
+        console.log(despesa);
+    }
 }
 
 let bd = new Bd();
@@ -129,4 +133,16 @@ function carregaListaDespesas(){
         linha.insertCell(3).innerHTML = d.valor;
     })
 
+}
+
+function pesquisarDespesas(){
+    let ano = document.getElementById('ano').value;
+    let mes = document.getElementById('mes').value;
+    let dia = document.getElementById('dia').value;
+    let tipo = document.getElementById('tipo').value;
+    let descricao = document.getElementById('descricao').value;
+    let valor = document.getElementById('valor').value;
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor);
+    bd.pesquisar(despesa);
 }
